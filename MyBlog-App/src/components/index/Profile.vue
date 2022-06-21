b84e08419e097d22ca829691d0cfe825a0a74d43
caieleven <1939470669@qq.com>
2022-06-21 13:55:54 +0800

    some change in front-end

---
+++ MyBlog-App/src/components/index/Profile.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <!-- 头像 -->
+    <div class="avatar">
+      <el-tooltip class="item" effect="dark" placement="right">
+        <div slot="content">你好啊！<br>只是我的博客</div>
+        <el-avatar :size="120" fit="contain">
+          <img :src="profile.avatar" alt="">
+        </el-avatar>
+      </el-tooltip>
+    </div>
+    <!-- 简介 -->
+    <div class="introduction">
+<!--      <p>{{ profile.nickname }}</p>-->
+      <p>夏季实训小组</p>
+    </div>
+    <!-- 社交 -->
+    <div class="social">
+      <a v-for="(scl,index) in social" :key="index" target="_blank" :href="scl.link">
+        <el-avatar :src="scl.icon" fit="cover" :title="scl.name">
+          {{ scl.name }}
+        </el-avatar>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Profile',
+  data() {
+    return {
+      social: [
+        // {
+        //   name: 'MyWeb',
+        //   link: 'https://refrain.xyz',
+        //   icon: 'https://ico.hnysnet.com/get.php?url=https://refrain.xyz'
+        // },
+        {
+          name: 'GitHub',
+          // link: 'https://github.com/snwjas',
+          link: 'https://github.com/caieleven',
+          icon: 'https://ico.hnysnet.com/get.php?url=https://github.com'
+        },
+        {
+          name: 'Gitee',
+          link: 'https://gitee.com/caieleven',
+          icon: 'https://ico.hnysnet.com/get.php?url=https://gitee.com'
+        },
+        // {
+        //   name: 'CSDN',
+        //   link: 'https://blog.csdn.net/u012131025',
+        //   icon: 'https://ico.hnysnet.com/get.php?url=https://www.csdn.net'
+        // }
+      ]
+    }
+  },
+  computed: {
+    ...mapGetters(['profile'])
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.avatar {
+  text-align: center;
+}
+
+.introduction {
+  text-align: center;
+  font-size: 1rem;
+  font-weight: 600;
+  margin-top: 0.75rem;
+}
+
+.social {
+  text-align: center;
+  margin-top: 1.2rem;
+
+  a {
+    margin-right: 7px;
+  }
+}
+</style>
+<style lang="scss">
+.el-avatar {
+  background: none;
+}
+</style>
+
